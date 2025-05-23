e0959985dd5f5b0d9f3e9ad152b437ba6934e59f
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-05-23 10:50:13 -0500

    Update main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -3,8 +3,9 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, usedPrefix, command }) => {
     let grupos = "*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* ⭐\n\n" +
                  "1-BajoBots\n" +
-                 "*✰* https://chat.whatsapp.com/Bi3Hy4y4RL8DyNyiH5OMEd" +
-                 "*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*\n\n" +
+                 "*✰* https://chat.whatsapp.com/Bi3Hy4y4RL8DyNyiH5OMEd"
+        +
+                 "  *─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*\n\n" +
                  "➠ Enlace anulado? entre aquí! \n\n" +
                  "⭐ Canal :\n" +
                  "*✰*https://whatsapp.com/channel/0029Vb63Kf9KwqSQLOQOtk3N" +
