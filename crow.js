be68e35b10841608f00d3d22a137690579066622
Wichi <sebastianbarbaro82@gmail.com>
2025-06-07 17:31:48 -0400

    Actualizar crow.js

--- crow.js
+++ crow.js
@@ -191,8 +191,7 @@ return msg?.message || ""
 },
 msgRetryCounterCache, // Resolver mensajes en espera
 msgRetryCounterMap, // Determinar si se debe volver a intentar enviar un mensaje o no
-defaultQueryTimeoutMs: undefined,
-version: [2, 3000, 1023223821],
+defaultQueryTimeoutMs: undefined,version: [2, 3000, 1023223821],
 }
 global.conn = makeWASocket(connectionOptions)
 if (!fs.existsSync(`./${authFile}/creds.json`)) {
